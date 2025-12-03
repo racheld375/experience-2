335263d3f3f767595df8a652b5ea3e1895b6b5c0
racheld375 <rd0556788375@gmail.com>
2025-12-03 19:53:54 +0200

    first version

---
+++ 1.js
@@ -0,0 +1,19 @@
+const fs=require("fs").promises
+const func=async()=>{
+    let snames=""
+    let sgrades=""
+    let data=await
+    fs.readFile("./names.txt","utf8")
+    snames=data
+    data=await
+    fs.readFile("./grades.txt","utf8")
+    sgrades=data
+    const arrnames=snames.split("\n")
+    const arrgrades=sgrades.split("\n")
+    const length=Math.min(arrgrades.length,arrnames.length)
+    for(let i=0;i<length;i++){
+        fs.appendFile("./new",arrnames[i]+" : "+arrgrades[i])
+    }
+    
+}
+func()
---
+++ 2.js
@@ -0,0 +1,6 @@
+const fs=require("fs").promises
+const func=async()=>{
+let data=await
+
+    }
+func()
